77e131f9d7e2795a9deaee13b82ad0fd6d5f5ea8
Rafagan Abreu <rafagansa@outlook.com>
2019-03-19 18:54:09 -0300

    Fixes, readme.md

--- README.md
+++ README.md
@@ -1,2 +1,15 @@
 # easy-vigenere-cipher
 Simple functions to encrypt and decrypt in C++ using Vigenere Cipher method
+
+Usage example:
+```C++
+constexpr auto text = "e+QCE)K<gjd{}-?D[}63~a#p$S-#4<%2%j)XV~8m_[]P;Vd)t7t@:M6;S";
+constexpr auto key = "jD7yeKQ,]udY$V5&Zyp%6MD4%^a-$_j}D>LdzqrYj['At+bv</cR:^-g8n";
+constexpr auto salt = -1; // Salt support to randomize even more the encryption
+auto encrypted = encrypt(text, key, salt);
+auto decrypted = decrypt(encrypted, key, salt);
+
+cout << encrypted << endl;
+cout << decrypted << endl;
+cout << (decrypted == string(text) ? "true" : "false") << endl;
+```
--- main.cpp
+++ main.cpp
@@ -22,14 +22,15 @@ int main() {
     printCipherMatrix();
     cout << endl;
 
-    auto text = string("e+QCE)K<gjd{}-?D[}63~a#p$S-#4<%2%j)XV~8m_[]P;Vd)t7t@:M6;S");
-    auto key = "jD7yeKQ,]udY$V5&Zyp%6MD4%^a-$_j}D>LdzqrYj['At+bv</cR:^-g8n";
-    auto encrypted = encrypt(text, key, 1);
-    auto decrypted = decrypt(encrypted, key, 1);
+    constexpr auto text = "e+QCE)K<gjd{}-?D[}63~a#p$S-#4<%2%j)XV~8m_[]P;Vd)t7t@:M6;S";
+    constexpr auto key = "jD7yeKQ,]udY$V5&Zyp%6MD4%^a-$_j}D>LdzqrYj['At+bv</cR:^-g8n";
+    constexpr auto salt = -1; // Salt support to randomize even more the encryption
+    auto encrypted = encrypt(text, key, salt);
+    auto decrypted = decrypt(encrypted, key, salt);
 
     cout << encrypted << endl;
     cout << decrypted << endl;
-    cout << (decrypted == text ? "true" : "false") << endl;
+    cout << (decrypted == string(text) ? "true" : "false") << endl;
 
     return 0;
 }
